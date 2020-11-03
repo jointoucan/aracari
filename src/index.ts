0342064daeb132c540e7784c18116c2d14871eda
Jacob Lowe <jacoblowe2.0@gmail.com>
2020-11-03 13:53:06 -0600

    Mapping/Address apis (#5)
    * add in new apis
    
    * fix test
    
    * add in case sensitive sp

--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,63 @@ interface Config {
   createTextNode?: (text: string) => Node;
 }
 
+interface ReplaceOptions {
+  at?: string
+}
+
+type Mapping = string[][];
+
 export class Aracari<T extends HTMLElement = HTMLElement> {
-  root: T;
+  root: T | undefined;
   mapping: string[][];
   config: Config;
 
-  constructor(root: T, options: Partial<Config> | undefined = {}) {
-    this.root = root;
+  constructor(root: T | Mapping, options: Partial<Config> | undefined = {}) {
     this.config = {
       textNodeType: options.textNodeType || Node.TEXT_NODE,
       createTextNode:
         options.createTextNode || document.createTextNode.bind(document)
     };
-    this.mapping = this.getTextNodeMapping(root);
+    if (Array.isArray(root)) {
+      this.mapping = root;
+    } else if (typeof root === "object" && root.childNodes) {
+      this.root = root;
+      this.mapping = this.getTextNodeMapping(root);
+    }
   }
 
   public getText() {
     return this.mapping.map(([text]) => text).join("");
   }
 
-  public isInSingleNode(text: string, caseSenative: boolean = true) {
-    return !!this.getTextNode(text, caseSenative);
+  public getAddressForText (text, caseSensitive: boolean = true): string | null {
+    const matchedNode = this.getMappingForText(text, caseSensitive);
+    return matchedNode ? matchedNode[1] : null;
   }
 
-  // TODO: this needs some options to do a couple of things
-  // find multiple instances of the work in a text node
-  // "and v. sand" be able to potentially pass regexp with a capture group
-  public getTextNode(
-    text: string,
-    caseSenative: boolean = true
-  ): ChildNode | undefined {
-    const pattern = new RegExp(text, `g${caseSenative ? "" : "i"}`);
-    const matchedNode = this.mapping.find(([text]) => !!text.match(pattern));
-    if (!matchedNode) return null;
-    const [, nodeMapping] = matchedNode;
-    // Get mapping and walk path to node.
-    const path = nodeMapping.split(".").map(i => parseInt(i, 10));
-    return this.walkNodes(this.root, path);
+  public getTextByAddress (address: string): string | null   {
+    const node = this.getMappingFromAddress(address);
+    return node ? node[0] : null;
   }
 
-  public replaceText(text: string, nodes: T | Node | (T | Node)[]) {
-    const node = this.getTextNode(text);
+  public isInSingleNode(text: string, caseSensitive: boolean = true) {
+    return !!this.getAddressForText(text, caseSensitive);
+  }
+
+  public getTextNode(text: string, caseSensitive: boolean = true) {
+    const address = this.getAddressForText(text, caseSensitive);
+    if (!address) return null;
+    return this.getNodeByAddress(address);
+  }
+
+  public replaceText(text: string, nodes: T | Node | (T | Node)[], options: ReplaceOptions = {}) {
+
+    let node;
+    if (options.at) {
+      node = this.getNodeByAddress(options.at);
+    } else {
+      node = this.getTextNode(text);
+    }
     if (!node) {
       return;
     }
@@ -60,9 +76,23 @@ export class Aracari<T extends HTMLElement = HTMLElement> {
     return this;
   }
 
-  public remap() {
-    this.mapping = this.getTextNodeMapping(this.root);
+  public remap(mapping?: Mapping) {
+    this.mapping = mapping ?? this.getTextNodeMapping(this.root);
     return this;
+  }
+
+  private getNodeByAddress (address: string) {
+    const path = address.split(".").map(i => parseInt(i, 10));
+    return this.walkNodes(this.root, path);
+  }
+
+  private getMappingForText (text: string, caseSensitive: boolean = true): string[] | undefined {
+    const pattern = new RegExp(text, `${caseSensitive ? 'i' : ''}g`);
+    return this.mapping.find(([text]) => !!text.match(pattern));
+  }
+
+  private getMappingFromAddress (address: string): string[] | undefined {
+    return this.mapping.find(([text, nodeAddress]) => nodeAddress === address);
   }
 
   private maybeCreateTextNode(text: string) {
